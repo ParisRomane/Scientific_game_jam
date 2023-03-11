e2e32aabc0622d3b95b66a4fecc1cd16bc005729
Yvregon <steph.ade17@gmail.com>
2023-03-11 21:44:16 +0100

    Character and bricks merged

---
+++ scripts/Player.gd
@@ -0,0 +1,135 @@
+extends CharacterBody2D
+
+var mouse_pos = Vector2()
+var vel = Vector2()
+var bullet = preload("res://scenes/bullet.tscn")
+var is_dead = false
+var can_shoot = true
+var shoot_line
+
+#Key var
+var right
+var left
+var up
+var down
+var move
+var shoot
+
+var ACC = 50
+
+#player stats
+var pv #int between 0 and pv_default
+var stat_speed # positive int
+var stat_damage # positive int
+var stat_regen # positive int
+var stat_mining # positive int
+
+@export var pv_default = 100 # 100 pv
+@export var regen_time_default = 1.0 # time to recover 1 pv in seconds
+@export var max_speed = 100
+@export var damage_default = 7 # harm to players
+@export var mining_default = 7 # harm to bocks
+
+signal player_death
+#signal player_grab_element(element)
+signal player_update_pv(pv)
+
+func _ready():
+	pv = 10
+	stat_regen = 0
+	stat_speed = 0
+	stat_damage = 0
+	stat_mining = 0
+
+
+func _physics_process(delta):	# 60 FPS (delta is in s)
+	if pv <= 0:
+		player_death.emit()
+	
+	action_loop()
+	set_velocity(vel)
+	move_and_slide()
+	vel = velocity
+	
+	update_pv(delta)
+
+func hit(damage):
+	pv -= damage
+
+var regen_clock = 0
+func update_pv(delta):
+	if stat_regen != 0:
+		# real time to recover 1 pv according to the regen stat
+		var regen_time = regen_time_default / stat_regen
+		
+		if regen_clock >= regen_time:
+			regen_clock = 0
+			if pv < pv_default:
+				pv +=1
+		
+		regen_clock += delta
+		
+		player_update_pv.emit(pv)
+
+func action_loop():
+	
+	right = Input.is_action_pressed("ui_right")
+	left = Input.is_action_pressed("ui_left")
+	up = Input.is_action_pressed("ui_up")
+	down = Input.is_action_pressed("ui_down")
+	move = right or left or up or down
+	shoot = Input.is_action_pressed("shoot")
+	
+	movement_loop()
+	shooting()
+
+func movement_loop():
+	var speed = max_speed * (1 + 0.2 * stat_speed)
+	
+	if !is_dead:
+		if right:
+			vel.x = min(vel.x + ACC, speed)
+		if left:
+			vel.x = max(vel.x - ACC, -speed)
+		if up:
+			vel.y = max(vel.y - ACC, -speed)
+		if down:
+			vel.y = min(vel.y + ACC, speed)
+	
+	#Inertia management
+	if !move or is_dead:
+		vel.x = lerpf(vel.x, 0, 0.15)
+		vel.y = lerpf(vel.y, 0, 0.15)
+
+func shooting():
+	
+	if shoot and can_shoot and !is_dead:
+		shoot_line = get_global_mouse_position() - global_position
+		var b = bullet.instantiate()
+		var rotation = shoot_line.angle()
+		can_shoot = false
+		
+		var damage = damage_default * (1 + 0.2 * stat_damage)
+		var mining = mining_default * (1 + 0.2 * stat_mining)
+		
+		#Bullet spawn
+		get_parent().add_child(b)
+		b.start($Arm/Marker2D.global_position, rotation, damage, mining)
+		
+		#Timer start
+		if $Reload.is_stopped():
+			$Reload.start()
+
+
+func _on_reload_timeout():
+	can_shoot = true
+	
+func upgrade(ind):
+	if ind == 0 :
+		stat_damage += 1
+	if ind == 1 :
+		stat_mining += 1
+	if ind == 2 :
+		stat_speed += 1
+	if ind == 3 :
+		stat_regen += 1
--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -2,19 +2,27 @@ extends CharacterBody2D
 
 const SPEED = 1000
 var vel = Vector2()
-var damage = 50
+var damage # harm in pv to player
+var mining # harm to block
 
-func start(pos, dir):
+func start(pos, dir, dam, min):
 	$Sprite2D.rotate(dir)
 	position = pos
+	damage = dam
+	mining = min
 	vel = Vector2(SPEED, 0).rotated(dir)
 
 func _physics_process(delta):
+	
 	var collision = move_and_collide(vel*delta)
+	var tilemap = get_parent().get_node("TileMap")
+	
 	if collision:
 		if collision.get_collider().has_method("hit"):
 			collision.get_collider().hit(damage);
+			
+		if collision.get_collider().name == "TileMap":
+			
+			tilemap.on_hit(collision, 10)
 		
-		if collision.get_collider().has_method("window"):
-			collision.get_collider().window(rad_to_deg(vel.angle()));
 		queue_free()
