671cc0388ac68b7ada2a0c9216d799b4469a1f5f
ParisRomane <romane.piing@gmail.com>
2023-04-01 12:31:06 +0200

    server udp fonctionnel

--- scripts/game.gd
+++ scripts/game.gd
@@ -15,16 +15,12 @@ func connect_ui_to_player(player):
 func _ready():
 	udp.connect_to_host("127.0.0.1", 4435)
 
+
 func _process(delta):
-	if !connected:
-		# Try to contact server
-		udp.put_packet("42".to_utf8_buffer())
-		print("here")
 	if udp.get_available_packet_count() > 0:
 		for i in range (udp.get_available_packet_count()) :
 			data = udp.get_packet()
 		data = data.get_string_from_utf8()
-		print(data)
 		connected = true
 		var info = JSON.parse_string(data.replace("'",'"'))
 		if (info != null) :
@@ -40,9 +36,9 @@ func update_data(datas):
 	if (datas != null):
 		for data in datas :
 			if ( data['name'] != player ):
-				get_node("Map/"+data['name']).change_setting(data['stat'])
+				#get_node("Map/"+data['name']).change_setting(data['stat'])
 				get_node("Map/"+data['name']).position = Vector2(data['position'][0],data['position'][1])
-				get_node("Map/"+data['name']).pv = data['pv']
+				#get_node("Map/"+data['name']).pv = data['pv']
 
 func send_update(position, pv, stat_speed, stat_damage, stat_regen, stat_range, name  ):
 	var data = {"name"= name, "position" = [position.x, position.y], "pv" = pv , "stat" = [ stat_speed, stat_damage, stat_regen, stat_range]}
